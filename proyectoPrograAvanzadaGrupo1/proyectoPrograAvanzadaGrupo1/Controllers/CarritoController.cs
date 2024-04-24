15fd304fcc4e6658b5d1e1ffbd9fd672c062aebe
Andres Leon Jimenez <105888687+AndresLeonJ@users.noreply.github.com>
2024-04-24 13:17:02 -0600

    Update CarritoController.cs

--- proyectoPrograAvanzadaGrupo1/proyectoPrograAvanzadaGrupo1/Controllers/CarritoController.cs
+++ proyectoPrograAvanzadaGrupo1/proyectoPrograAvanzadaGrupo1/Controllers/CarritoController.cs
@@ -12,5 +12,146 @@ namespace proyectoPrograAvanzadaGrupo1.Controllers
         {
             _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            var carrito = await _context.Carrito
+                .Include(c => c.Usuario)
+                .Include(c => c.Producto)
+                .ToListAsync();
+
+            return View(carrito);
+        }
+
+        public async Task<IActionResult> Detalles(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var carrito = await _context.Carrito
+                .Include(c => c.Usuario)
+                .Include(c => c.Producto)
+                .FirstOrDefaultAsync(m => m.CarritoId == id);
+
+            if (carrito == null)
+            {
+                return NotFound();
+            }
+
+            return View(carrito);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear([Bind("CarritoId,UserId,ProductoId,Cantidad")] Carrito carrito)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carrito);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carrito);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carrito = await _context.Carrito.FindAsync(id);
+            if (carrito == null)
+            {
+                return NotFound();
+            }
+            return View(carrito);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CarritoId,UserId,ProductoId,Cantidad")] Carrito carrito)
+        {
+            if (id != carrito.CarritoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carrito);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarritoExiste(carrito.CarritoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carrito);
+        }
+
+ 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carrito = await _context.Carrito
+                .Include(c => c.Usuario)
+                .Include(c => c.Producto)
+                .FirstOrDefaultAsync(m => m.CarritoId == id);
+            if (carrito == null)
+            {
+                return NotFound();
+            }
+
+            return View(carrito);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carrito = await _context.Carrito.FindAsync(id);
+            _context.Carrito.Remove(carrito);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CarritoExiste(int id)
+        {
+            return _context.Carrito.Any(e => e.CarritoId == id);
+        }
+        public decimal ObetenerTotal()
+        {
+            var ItemsCarrito = _context.Carrito.Include(c => c.Producto).ToList();
+
+            decimal total = 0;
+
+            foreach (var item in ItemsCarrito)
+            {
+                total += item.Cantidad * item.Producto.precio;
+            }
+
+            return total;
+        }
+
     }
 }
